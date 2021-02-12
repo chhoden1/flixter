562c4755de6d5ddc97bc67dec59f67879dcd96b9
chhodensherpa <76025934+chhodensherpa@users.noreply.github.com>
2021-02-12 13:27:25 -0800

    Worked on improving the positioning for Poster view

--- flixter/MoviesViewController.swift
+++ flixter/MoviesViewController.swift
@@ -22,7 +22,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.delegate = self
 
         // Do any additional setup after loading the view.
-        print("Hello")
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -32,11 +31,10 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
               print(error.localizedDescription)
            } else if let data = data {
               let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-            
-            self.movies=dataDictionary["results"] as! [[String:Any]]
-            
-            self.tableView.reloadData()
             print(dataDictionary)
+            self.movies=dataDictionary["results"] as! [[String:Any]]
+            self.tableView.reloadData()
+           // print(dataDictionary)
             
 
               // TODO: Get the array of movies
@@ -57,7 +55,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
-        
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
         let synopsis = movie["overview"] as! String
@@ -66,11 +63,14 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         cell.synopsisLabel.text = synopsis
         let baseURL = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
-        let posterURL = URL(string:baseURL+posterPath)!
+        let posterURL = URL(string: baseURL + posterPath)!
         
         
         cell.posterView.af_setImage(withURL: posterURL)
         return cell
+    }
+    func tableView(_tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
     /*
